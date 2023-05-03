f3cf149ba49822b5337c74cbc91413c755069958
constanza.giacoboni <constanza.giacoboni@whitebox.com.ar>
2023-05-03 17:15:16 -0300

    ejercicio 5 completo

--- TP02/Ejercicio05.hs
+++ TP02/Ejercicio05.hs
@@ -6,4 +6,21 @@ factorial n
     | otherwise   = 0
 
 
-sumatoria::
+sucesion:: Int -> Int -> Float
+sucesion x n = fromIntegral (numerador(n)) / (fromIntegral (factorial(x)))
+
+numerador:: Int -> Int
+numerador n = sumatoria 1 n
+
+sumatoria:: Int -> Int -> Int
+sumatoria i n
+    | i == n    = potencia n n
+    | i < n     = (potencia i n) + (sumatoria (i + 1) n)
+    | otherwise = 0
+
+potencia:: Int -> Int -> Int
+potencia x y
+    | y == 0    = 1
+    | y == 1    = x
+    | y > 1     = (potencia x (y - 1)) + x
+    | otherwise = 0
